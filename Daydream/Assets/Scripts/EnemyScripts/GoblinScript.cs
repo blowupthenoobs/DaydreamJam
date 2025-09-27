81d22f78697d2d3c3abaae10e33b28f7902b45a5
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2025-09-27 15:56:32 -0400

    Finished the Gobbo

--- Daydream/Assets/Scripts/EnemyScripts/GoblinScript.cs
+++ Daydream/Assets/Scripts/EnemyScripts/GoblinScript.cs
@@ -1,26 +1,64 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GoblinScript : EnemyScript
 {
+    private bool charging;
+    [SerializeField] float chargeSpeed;
+    private float currentSpeed;
+    [SerializeField] float decellerationSpeed;
+    private Vector3 chargeDirection;
+
+    void Update()
+    {
+        Move();
+    }
+
     protected void Move()
     {
-        if (PlayerInLOS())
+        if (!charging)
         {
-            if (Vector2.Distance(transform.position, target.transform.position) > followDistance)
+            if (PlayerInLOS())
             {
-                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+                if(Vector2.Distance(transform.position, target.transform.position) > followDistance)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+                }
+                if(Vector2.Distance(transform.position, target.transform.position) < attackRange)
+                {
+                    Attack();
+                }
             }
-            if (Vector2.Distance(transform.position, target.transform.position) < attackRange)
-            {
-                Attack();
-            }
+            else
+                Wander();
         }
         else
-            Wander();
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (chargeDirection * 100), currentSpeed * Time.deltaTime);
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, decellerationSpeed * Time.deltaTime);
+
+            if(currentSpeed <= 5)
+                currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, decellerationSpeed * Time.deltaTime);
+            if(currentSpeed <= 2)
+                charging = false;
+        }
     }
 
     protected void Attack()
     {
-        
+        if (currentCooldown >= cooldown)
+        {
+            charging = true;
+            currentSpeed = chargeSpeed;
+            chargeDirection = (target.transform.position - transform.position).normalized;
+            currentCooldown = 0;
+        }
+        else
+            currentCooldown += Time.deltaTime;
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        charging = false;
     }
 }
